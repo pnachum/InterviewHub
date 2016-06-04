221067ec4a50a7743b02e652c36c6e6fb0670d3e
Phil Nachum <phil.nachum@airbnb.com>
2016-06-04 11:08:59 -0700

    pass user ID to isUserAdmin instead of weird this binding

--- imports/api/questions/questions.js
+++ imports/api/questions/questions.js
@@ -4,9 +4,9 @@ import { check } from 'meteor/check';
 
 export const Questions = new Mongo.Collection('Questions');
 
-function isUserAdmin() {
+function isUserAdmin(userId) {
   // Shitty global refernce to alanning/meteor-roles
-  return this.userId && Roles.userIsInRole(this.userId, ['admin']);
+  return userId && Roles.userIsInRole(userId, ['admin']);
 }
 
 if (Meteor.isServer) {
@@ -15,7 +15,7 @@ if (Meteor.isServer) {
   });
 
   Meteor.publish('questions.all', function allQuestionsPublication() {
-    if (isUserAdmin.call(this)) {
+    if (isUserAdmin(this.userId)) {
       return Questions.find({});
     } else {
       this.stop();
@@ -25,7 +25,7 @@ if (Meteor.isServer) {
 
   Meteor.publish('question', function questionPublication(questionId) {
     const question = Questions.findOne(questionId);
-    if (question.status === 'approved' || isUserAdmin.call(this)) {
+    if (question.status === 'approved' || isUserAdmin(this.userId)) {
       return Questions.find(questionId);
     } else {
       this.stop();
@@ -56,7 +56,7 @@ Meteor.methods({
   'questions.remove'(questionId) {
     check(questionId, String);
 
-    if (isUserAdmin.call(this)) {
+    if (isUserAdmin(this.userId)) {
       Questions.remove(questionId);
     } else {
       throw new Meteor.Error('not-authorized');
@@ -67,7 +67,7 @@ Meteor.methods({
     check(questionId, String);
     check(data, Object);
 
-    if (isUserAdmin.call(this)) {
+    if (isUserAdmin(this.userId)) {
       Questions.update(questionId, { $set: data });
     } else {
       throw new Meteor.Error('not-authorized');
